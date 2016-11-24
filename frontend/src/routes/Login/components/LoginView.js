33b5b3e1ea5964e48614c78f843a96b04079738b
Vincent Chavez <vchavez@pearup.com>
2016-11-24 12:56:18 -0500

    Added the login page component

---
+++ frontend/src/routes/Login/components/LoginView.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react'
+
+class LoginView extends Component {
+	static propTypes = {
+
+	}
+
+	render () {
+
+		return (
+			<div>
+				<h4>Login</h4>
+			</div>
+		)
+	}
+}
+
+export default LoginView
