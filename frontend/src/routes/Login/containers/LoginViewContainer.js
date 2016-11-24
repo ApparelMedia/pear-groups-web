5bd33900009cddf24a918b1b973050e180c9df3e
Vincent Chavez <vchavez@pearup.com>
2016-11-24 12:56:34 -0500

    Added the login page container

---
+++ frontend/src/routes/Login/containers/LoginViewContainer.js
@@ -0,0 +1,12 @@
+import LoginView from '../components/LoginView'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginView)
