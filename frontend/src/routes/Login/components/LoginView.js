3aee2725fffc4d753c530d5fa48c026b94bcf303
Vincent Chavez <vchavez@pearup.com>
2016-12-02 18:02:48 -0500

    Added the locate button to the container

--- frontend/src/routes/Login/components/LoginView.js
+++ frontend/src/routes/Login/components/LoginView.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { Button } from 'antd'
 
 class LoginView extends Component {
-  static propTypes = {}
+  static propTypes = {
+    locateMeOnClick: PropTypes.func.isRequired,
+    city: PropTypes.string,
+    state: PropTypes.string
+  }
+
+  getLocation (city, state) {
+    if (city && state) {
+      return <p>City: {city} - State: {state}</p>
+    }
+
+    return '';
+  }
 
   render () {
+    const { locateMeOnClick, city, state } = this.props
+
+    let location = this.getLocation(city, state)
+
     return (
       <div>
         <h4>Login</h4>
+        {location}
+        <Button onClick={locateMeOnClick}>Locate Me</Button>
       </div>
     )
   }
--- frontend/src/routes/Login/containers/LoginViewContainer.js
+++ frontend/src/routes/Login/containers/LoginViewContainer.js
@@ -1,12 +1,17 @@
 import LoginView from '../components/LoginView'
 import { connect } from 'react-redux'
+import { requestLocateUser } from 'actions/login'
 
 const mapStateToProps = (state) => ({
-
+  city: state.user.city,
+  state: state.user.state
 })
 
-const mapDispatchToProps = {
-
-}
+const mapDispatchToProps = (dispatch) => ({
+  locateMeOnClick: (event) => {
+    event.preventDefault()
+    dispatch(requestLocateUser())
+  }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginView)
